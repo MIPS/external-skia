b413491ffd7d31272fe8693e102aa205401015ed
Raghu Gandham <raghu@mips.com>
2010-11-17 12:47:12 -0800

    [MIPS]Rewrite S32_D565_Blend_mips routine and add EB support
    Change-Id: I1e8eacc54ecc718884561bcc3510a09d9d241544
    Signed-off-by: Raghu Gandham <raghu@mips.com>

--- src/opts/SkBlitRow_opts_mips.cpp
+++ src/opts/SkBlitRow_opts_mips.cpp
@@ -2,107 +2,109 @@
 #include "SkColorPriv.h"
 #include "SkDither.h"
 
-#if defined ( __mips_dsp) && defined(SK_CPU_LENDIAN)
+#if defined ( __mips_dsp)
 static void S32_D565_Blend_mips(uint16_t* SK_RESTRICT dst,
 				const SkPMColor* SK_RESTRICT src, int count,
 				U8CPU alpha, int /*x*/, int /*y*/) {
-
-	asm volatile (
-		"addiu	$sp, $sp, -16		\n\t"
-		"sw	$s0, 4($sp)		\n\t"
-		"sw	$s1, 8($sp)		\n\t"
-		"sw	$s2, 12($sp)		\n\t"
-		"sw	$s3, 16($sp)		\n\t"
-		"addiu	$a3, $a3, 1		\n\t"
-		"blt	$a2, 2, loopend		\n\t"
-		"sll	$s4, $a3, 8		\n\t"/*mult vector is used later*/
-		"or	$s4, $s4, $a3		\n\t"
-		"loopB:				\n\t"
-		"lwr	$s0, 0($a0)		\n\t"
-		"lwl	$s0, 3($a0)		\n\t"
-		"lw	$s2, 0($a1)		\n\t"
-		"lw	$s3, 4($a1)		\n\t"
-		"and	$t1, $s0, 0x001f001f	\n\t"/*Get two blues from two Dst pixels*/
-		"shra.ph $t0, $s0, 5		\n\t"/*Get two greens from two Dst pixels*/
-		"and	$t2, $t0, 0x003f003f	\n\t"
-		"shra.ph $t0, $s0, 11		\n\t"/*Get two reds from two Dst pixels*/
-		"and	$t3, $t0, 0x001f001f	\n\t"
-		"precrq.ph.w $t0, $s2, $s3	\n\t"
-		"shrl.qb $t5, $t0, 3		\n\t"
-		"and	$t4, $t5, 0x001f001f	\n\t"/*t4 has two blues from src*/
-		"ins	$s2, $s3, 16, 16	\n\t"
-		"and	$t0, $s2, 0x00ff00ff	\n\t"
-		"shrl.qb $t6, $t0, 3		\n\t"/*t6 has got two reds from src*/
-		"shra.ph $t0, $s2, 0xa		\n\t"
-		"and	$t5, $t0, 0x003f003f	\n\t"/*t5 has got two greens from src*/
-		"subu.qb	$t7, $t4, $t1	\n\t"
-		"subu.qb	$t8, $t5, $t2	\n\t"
-		"subu.qb	$t9, $t6, $t3	\n\t"
-		"muleu_s.ph.qbr	$t4, $s4, $t7	\n\t"
-		"muleu_s.ph.qbr	$t5, $s4, $t8	\n\t"
-		"muleu_s.ph.qbr	$t6, $s4, $t9	\n\t"
-		"shra.ph	$t7, $t4, 8	\n\t"
-		"shra.ph	$t8, $t5, 8	\n\t"
-		"shra.ph	$t9, $t6, 8	\n\t"
-		"addu.qb	$t4, $t7, $t1	\n\t"
-		"addu.qb	$t5, $t8, $t2	\n\t"
-		"addu.qb	$t6, $t9, $t3	\n\t"
-		"ext	$s0, $t4, 3, 5		\n\t"/* set the blue into Dst */
-		"ext	$t0, $t5, 2, 6		\n\t"/* set the Green into Dst*/
-		"ins	$s0, $t0, 5, 6 		\n\t"
-		"ext	$t0, $t6, 3, 5		\n\t"/* set the Red into Dst */
-		"ins	$s0, $t0, 11, 5		\n\t"
-		"sh	$s0, 0($a0)		\n\t"/* Store s0 */
-		"ext	$s1, $t4, 19, 5		\n\t"
-		"ext	$t0, $t5, 18, 6		\n\t"
-		"ins	$s1, $t0, 5, 6		\n\t"
-		"ext	$t0, $t6, 19, 5		\n\t"
-		"ins	$s1, $t0, 11, 5		\n\t"
-		"sh	$s0, 2($a0)		\n\t"
-		"addiu	$a2, $a2, -2		\n\t"
-		"addiu	$a1, $a1, 8		\n\t"
-		"addiu	$a0, $a0, 4		\n\t"
-		"bge	$a2, 2, loopB		\n\t"
-		"loopend:			\n\t"
-		"blez	$a2, end		\n\t"
-		"lhu	$v1, 0($a0)		\n\t"
-		"lw	$v0, 0($a1)		\n\t"
-		"addiu  $a2, $a2, -2		\n\t"
-		"srl	$t1, $v1, 0xb		\n\t"
-		"ext	$t0, $v1, 0x5, 0x6	\n\t"
-		"ext	$t9, $v0, 0x3, 0x5	\n\t"
-		"ext	$t8, $v0, 0xa, 0x6	\n\t"
-		"subu	$t7, $t9, $t1		\n\t"
-		"subu	$t6, $t8, $t0		\n\t"
-		"mul	$t5, $t7, $a3		\n\t"
-		"mul	$t4, $t6, $a3		\n\t"
-		"sra	$t3, $t5, 0x8		\n\t"
-		"sra	$t2, $t4, 0x8		\n\t"
-		"addu	$t8, $t3, $t1		\n\t"
-		"addu	$t6, $t2, $t0		\n\t"
-		"ext	$t9, $v0, 0x13, 0x5	\n\t"
-		"andi	$t2, $v1, 0x1f		\n\t"
-		"subu	$t7, $t9, $t2		\n\t"
-		"sll	$t4, $t8, 0xb		\n\t"
-		"mul	$t5, $t7, $a3		\n\t"
-		"sll	$t3, $t6, 0x5		\n\t"
-		"sra	$v0, $t5, 0x8		\n\t"
-		"or	$t0, $t4, $t3		\n\t"
-		"addu	$t1, $v0, $t2		\n\t"
-		"or	$v1, $t0, $t1		\n\t"
-		"sh	$v1, 0($a0)		\n\t"
-		"end:				\n\t"
-		"lw	$s0, 4($sp) 		\n\t"
-		"lw	$s1, 8($sp) 		\n\t"
-		"lw	$s2, 12($sp) 		\n\t"
-		"lw	$s3, 16($sp) 		\n\t"
-		"jr	$ra			\n\t"
-		"addiu	$sp, $sp, 16		\n\t"
-		:
-		:
-		:"t0","t1","t2","t3","t4","t5","t6","t7","t8","t9",
-                  "s0","s1","s2","s3","s4","a0","a1","a2","a3","v0","v1"
-		);
+	register uint32_t t0, t1, t2, t3, t4, t5, t6, t7, t8;
+	register uint32_t t9, s0, s1, s2, s3, s4, s5, s6, s7, v0, v1;
+	alpha +=1;
+	if (count >= 2) {
+	  asm volatile (
+		"sll %[s4], %[alpha], 8			\n\t"/*mult vector is used later*/
+		"or %[s4], %[s4], %[alpha]		\n\t"
+		"repl.ph %[s5], 0x1f			\n\t"
+		"repl.ph %[s6], 0x3f			\n\t"
+		"repl.ph %[s7], 0x3f			\n\t"
+		"1:					\n\t"
+		"lw	%[s2], 0(%[src])		\n\t"
+		"lw	%[s3], 4(%[src])		\n\t"
+#if defined	(SK_CPU_LENDIAN)
+		"lwr	%[s0], 0(%[dst])		\n\t"
+		"lwl	%[s0], 3(%[dst])		\n\t"
+		"and	%[t1], %[s0], %[s5]		\n\t"/*Get two blues from two Dst pixels*/
+		"shra.ph %[t0], %[s0], 5		\n\t"/*Get two greens from two Dst pixels*/
+		"and	%[t2], %[t0], %[s6]		\n\t"
+		"shra.ph %[t0], %[s0], 11		\n\t"/*Get two reds from two Dst pixels*/
+		"and	%[t3], %[t0], %[s5]		\n\t"
+		"precrq.ph.w %[t0], %[s3], %[s2]	\n\t"
+		"shrl.qb %[t5], %[t0], 3		\n\t"
+		"and	%[t4], %[t5], %[s5]		\n\t"/*t4 has two blues from src*/
+		"ins	%[s2], %[s3], 16, 16		\n\t"
+		"and	%[t0], %[s2], %[s7]		\n\t"
+		"shrl.qb %[t6], %[t0], 3		\n\t"/*t6 has got two reds from src*/
+		"shra.ph %[t0], %[s2], 10		\n\t"
+		"and	%[t5], %[t0], %[s6]		\n\t"/*t5 has got two greens from src*/
+#else
+		"lwl	%[s0], 0(%[dst])		\n\t"
+		"lwr	%[s0], 3(%[dst])		\n\t"
+		"and    %[t3], %[s0], %[s5]		\n\t"/*Get two reds from two Dst pixels*/
+		"shra.ph %[t0], %[s0], 5                \n\t"/*Get two greens from two Dst pixels*/
+		"and    %[t2], %[t0], %[s6]		\n\t"
+		"shra.ph %[t0], %[s0], 11               \n\t"/*Get two blues from two Dst pixels*/
+		"and    %[t1], %[t0], %[s5]		\n\t"
+		"precrq.ph.w %[t0], %[s3], %[s2]	\n\t"
+		"shrl.qb %[t4], %[t0], 2		\n\t"
+		"and	%[t5], %[t4], %[s6]		\n\t"/*t5 has two greens from src*/
+		"ins    %[s2], %[s3], 16, 16            \n\t"
+		"shra.ph %[t4], %[t0], 11		\n\t"/*t6 has got two reds from src*/
+		"and	%[t6], %[t4], %[s5]		\n\t"
+		"shra.ph %[t0], %[s2], 11		\n\t"
+		"and    %[t4], %[t0], %[s5]	        \n\t"/*t4 has two blues from src*/
+#endif
+		"subu.qb	%[t7], %[t4], %[t1]	\n\t"
+		"subu.qb	%[t8], %[t5], %[t2]	\n\t"
+		"subu.qb	%[t9], %[t6], %[t3]	\n\t"
+		"muleu_s.ph.qbr	%[t4], %[s4], %[t7]	\n\t"
+		"muleu_s.ph.qbr	%[t5], %[s4], %[t8]	\n\t"
+		"muleu_s.ph.qbr	%[t6], %[s4], %[t9]	\n\t"
+		"shra.ph	%[t7], %[t4], 8		\n\t"
+		"shra.ph	%[t8], %[t5], 8		\n\t"
+		"shra.ph	%[t9], %[t6], 8		\n\t"
+		"addu.qb	%[t4], %[t7], %[t1]	\n\t"
+		"addu.qb	%[t5], %[t8], %[t2]	\n\t"
+		"addu.qb	%[t6], %[t9], %[t3]	\n\t"
+		"andi	%[s0], %[t4], 0xffff		\n\t"
+		"andi	%[t0], %[t5], 0xffff    	\n\t"
+		"sll	%[t0], %[t0], 0x5		\n\t"
+		"or	%[s0], %[s0], %[t0]		\n\t"
+		"sll	%[t0], %[t6], 0xb		\n\t"
+		"or	%[t0], %[t0], %[s0]		\n\t"
+		"sh	%[t0], 0(%[dst])		\n\t"
+		"srl	%[s1], %[t4], 16		\n\t"
+		"srl    %[t0], %[t5], 16                \n\t"
+		"sll    %[t5], %[t0], 5                 \n\t"
+		"or	%[t0], %[t5], %[s1] 		\n\t"
+		"srl	%[s0], %[t6], 16		\n\t"
+		"sll	%[s2], %[s0], 0xb		\n\t"
+		"or	%[s1], %[s2], %[t0]		\n\t"
+		"sh	%[s1], 2(%[dst])		\n\t"
+		"addiu	%[count], %[count], -2		\n\t"
+		"addiu	%[src], %[src], 8		\n\t"
+		"addiu	%[dst], %[dst], 4		\n\t"
+		"bge	%[count], 2, 1b			\n\t"
+		:[t0]"=&r"(t0), [t1]"=&r"(t1), [t2]"=&r"(t2), [t3]"=&r"(t3), [t4]"=&r"(t4),
+		[t5]"=&r"(t5), [t6]"=&r"(t6), [t7]"=&r"(t7), [t8]"=&r"(t8), [t9]"=&r"(t9),
+		[s0]"=&r"(s0), [s1]"=&r"(s1), [s2]"=&r"(s2), [s3]"=&r"(s3), [s4]"=&r"(s4),
+		[s5]"=&r"(s5), [s6]"=&r"(s6), [s7]"=&r"(s7), [v0]"=&r"(v0), [v1]"=&r"(v1), 
+		[count]"+r"(count)
+		:[alpha]"r"(alpha), [dst]"r"(dst), [src]"r"(src)
+		:"hi", "lo", "memory"
+	  );
+	}
+	if (count == 1) {
+		SkPMColor c = *src++;
+		SkPMColorAssert(c);
+		SkASSERT(SkGetPackedA32(c) == 255);
+		uint16_t d = *dst;
+		*dst++ = SkPackRGB16( SkAlphaBlend(SkPacked32ToR16(c),
+				SkGetPackedR16(d), alpha),
+				SkAlphaBlend(SkPacked32ToG16(c),
+				SkGetPackedG16(d), alpha),
+				SkAlphaBlend(SkPacked32ToB16(c),
+				SkGetPackedB16(d),
+				alpha));
+	}
 }
 
 static void S32A_D565_Opaque_Dither_mips(uint16_t* __restrict__ dst,
