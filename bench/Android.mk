6b71da9b48bdbd93dc5402a7c2f4cd0217c1d6a4
Damir Tatalovic <damir.tatalovic@rt-rk.com>
2010-12-01 18:28:42 +0100

    [MIPS] Added skia_bench extension for benchmarking Filter_32, Matrix and BlitRow routines
    Change-Id: Id85c3466e325567ac964a77a45822019c14c7a78

--- bench/Android.mk
+++ bench/Android.mk
@@ -3,14 +3,15 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	BitmapBench.cpp \
+    BitmapBench.cpp \
     DecodeBench.cpp \
     FPSBench.cpp \
-	RectBench.cpp \
-	RepeatTileBench.cpp \
-	TextBench.cpp \
-	SkBenchmark.cpp \
-	benchmain.cpp
+    RectBench.cpp \
+    RepeatTileBench.cpp \
+    TextBench.cpp \
+    BlitBench.cpp \
+    SkBenchmark.cpp \
+    benchmain.cpp
 
 # additional optional class for this tool
 LOCAL_SRC_FILES += \
