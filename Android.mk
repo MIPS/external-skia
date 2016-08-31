4f1957e79f44fb488be611176fbf7b503f8a7e27
Jovan Zelincevic <jovan.zelincevic@rt-rk.com>
2016-08-31 14:36:45 +0200

    Build fix
    Fixing build for TARGET_ARCH_VARIANT=mips32r2dspr2-fp
    and TARGET_ARCH_VARIANT=mips32r2dsp-fp
    
    (cherry picked from commit 282f884b4570af41dec90ce18c5ddfde8a9c32af)
    
    Change-Id: Ia44f8e7463cefcc7f9857f818742652b0aba8108

--- Android.mk
+++ Android.mk
@@ -789,10 +789,30 @@ LOCAL_SRC_FILES_x86_64 += \
 LOCAL_CFLAGS_mips += \
 	-EL
 
+ifeq ($(TARGET_ARCH_VARIANT), mips32r2dspr2-fp)
+LOCAL_CFLAGS_mips += \
+	-DSK_MIPS_HAS_DSPR2
+
+LOCAL_SRC_FILES_mips += \
+	src/opts/SkBitmapProcState_opts_mips_dsp.cpp \
+	src/opts/SkBlitRow_opts_mips_dsp.cpp \
+	src/opts/SkBlitMask_opts_none.cpp
+
+else ifeq ($(TARGET_ARCH_VARIANT), mips32r2dsp-fp)
+LOCAL_CFLAGS_mips += \
+	-DSK_MIPS_HAS_DSP
+
+LOCAL_SRC_FILES_mips += \
+	src/opts/SkBitmapProcState_opts_mips_dsp.cpp \
+	src/opts/SkBlitRow_opts_mips_dsp.cpp \
+	src/opts/SkBlitMask_opts_none.cpp
+
+else
 LOCAL_SRC_FILES_mips += \
 	src/opts/SkBitmapProcState_opts_none.cpp \
 	src/opts/SkBlitMask_opts_none.cpp \
 	src/opts/SkBlitRow_opts_none.cpp
+endif
 
 LOCAL_CFLAGS_mips64 += \
 	-EL
